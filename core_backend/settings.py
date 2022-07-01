594861a0d4b59a977248221d1c953707e9d0c6e6
NeielJair <nurbano@mahisoft.com>
2022-07-01 15:50:23 -0300

    Connection to front example

--- core_backend/settings.py
+++ core_backend/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PARENT_DIR = BASE_DIR.parent
+FRONTEND_DIR = os.environ.get('FRONTEND_DIR', os.path.join(PARENT_DIR, 'core_frontend'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'core_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [os.path.join(FRONTEND_DIR, 'build')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -68,6 +70,8 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_DIRS = [os.path.join(FRONTEND_DIR, 'build', 'static')]
 
 WSGI_APPLICATION = 'core_backend.wsgi.application'
 
--- core_backend/urls.py
+++ core_backend/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core_api.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', front),
 ]
