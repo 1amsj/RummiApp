b32f46319df96f663c914f5f2ae3fa29fc0e878a
NeielJair <nurbano@mahisoft.com>
2022-07-12 10:36:34 -0300

    Added test endpoint

--- core_api/urls.py
+++ core_api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from . import views
 
@@ -9,5 +7,6 @@ urlpatterns = [
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterView.as_view(), name='auth_register'),
+    path('test/', views.test_end_point, name='test'),
     path('', views.get_routes)
 ]
