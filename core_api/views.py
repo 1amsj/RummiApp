5b36849b8e5a5bfaec958a0f70548b55a3effe15
NeielJair <nurbano@mahisoft.com>
2022-07-12 10:24:44 -0300

    Update api

--- core_api/views.py
+++ core_api/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from core_api.serializers import MyTokenObtainPairSerializer, RegisterSerializer
 
-def front(request):
-    return render(request, "index.html", {})
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        'token/',
+        'register/',
+        'token/refresh/'
+    ]
+    return Response(['api/v1/' + r for r in routes])
