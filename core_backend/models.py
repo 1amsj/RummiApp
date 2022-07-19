8b5d2b5bc8042496206c162e01a55ea13756a1c1
NeielJair <nurbano@mahisoft.com>
2022-07-19 11:26:35 -0300

    Made user subcategories link to user

--- core_backend/models.py
+++ core_backend/models.py
@@ -23,22 +23,22 @@ class User(AbstractUser):
 
 class Provider(models.Model):
     """Who provides the service"""
-    user = models.OneToOneField(User, models.CASCADE, 'as_provider', primary_key=True)
+    user = models.OneToOneField(User, models.CASCADE, 'as_provider', primary_key=True, parent_link=True)
 
 
 class Consumer(models.Model):
     """Who receives the service case"""
-    user = models.OneToOneField(User, models.CASCADE, 'as_consumer', primary_key=True)
+    user = models.OneToOneField(User, models.CASCADE, 'as_consumer', primary_key=True, parent_link=True)
 
 
 class Payer(models.Model):
     """Who pays the service bill"""
-    user = models.OneToOneField(User, models.CASCADE, 'as_payer', primary_key=True)
+    user = models.OneToOneField(User, models.CASCADE, 'as_payer', primary_key=True, parent_link=True)
 
 
 class Agent(models.Model):
     """Who requests the service case for the Customer"""
-    user = models.OneToOneField(User, models.CASCADE, 'as_agent', primary_key=True)
+    user = models.OneToOneField(User, models.CASCADE, 'as_agent', primary_key=True, parent_link=True)
 
 
 # Service models
