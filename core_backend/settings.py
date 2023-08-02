aec7719fe0419a62da3b51b803047d615a7da33b
Diego Rangel <starloss09@gmail.com>
2023-08-02 17:16:57 -0400

    Static INFO_FILE_DIR added

--- core_backend/settings.py
+++ core_backend/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 PARENT_DIR = BASE_DIR.parent
 STATIC_ROOT = os.environ.get('FRONTEND_DIR', os.path.join(PARENT_DIR, 'core_frontend'))
 VERSION_FILE_DIR = os.path.join(BASE_DIR, 'logs', 'latest_commit.txt')
+INFO_FILE_DIR = os.path.join(BASE_DIR, 'logs', 'info.log')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -213,7 +214,7 @@ LOGGING = {
         'file': {
             'level': 'INFO',
             'class': 'logging.FileHandler',
-            'filename': 'info.log',
+            'filename': INFO_FILE_DIR,
             'formatter': 'verbose'
         },
         'console': {
