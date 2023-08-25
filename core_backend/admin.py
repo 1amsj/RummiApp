9fe3db69899bc2e5123992193a976cd397f55f88
Starloss <43561472+Starloss@users.noreply.github.com>
2023-08-25 11:43:44 -0400

    Update core_backend/admin.py
    Co-authored-by: Neiel Urbano <53912324+NeielJair@users.noreply.github.com>

--- core_backend/admin.py
+++ core_backend/admin.py
@@ -124,7 +124,7 @@ basic_register(Booking, extendable=True, admin_inlines=[stacked_inline(Ledger), 
 basic_register(Event, extendable=True, admin_inlines=[stacked_inline(Authorization.events.through)])
 basic_register(Expense)
 basic_register(Offer)
-basic_register(Report, historical=True)
+basic_register(Report)
 
 basic_register(Authorization, readonly=('last_updated_at',))
 basic_register(Extra)
