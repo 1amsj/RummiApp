3da3bdec73b76fca453755e203c6fd1563ac7600
Neiel Urbano <53912324+NeielJair@users.noreply.github.com>
2023-09-06 14:11:21 -0300

    Revert "Booking public ID is now taken from event and in PST (#120)" (#121)
    This reverts commit eb19051d6f6a19a6a415d0f5317f9253bb72b57d.

--- core_backend/fields.py
+++ core_backend/fields.py
@@ -1,31 +1,26 @@
+from datetime import datetime
 from django.db import models
-from pytz import timezone
-
 
 class DailyUniqueIdentifierField(models.CharField):
     # This is a pre_save, therefore this will not be called on bulk_create, a for loop with .save() should be used instead
     def pre_save(self, model_instance, add):
-        if not add:
-            return model_instance.__dict__[self.name]
+        if add:
+            # Get the current date
+            date = datetime.now().date()
+            
+            # Get the last object with the same date prefix
+            querySet = model_instance.__class__.objects.filter(**{
+                F"{self.name}__startswith": date.strftime('%y%m%d')
+            })
+            last_object = querySet.order_by(F"-{self.name}").first()
 
-        # Get date in pst, this will only work for Booking model
-        pst = timezone('US/Pacific')
-        date_utc = model_instance.events.first().start_at
-        date_pst = date_utc.astimezone(pst)
+            # Increment the sequence number
+            sequence_number = 1
+            if last_object:
+                last_sequence_number = int(last_object.__dict__[self.name][9:])
+                sequence_number = last_sequence_number + 1
 
-        # Get the last object with the same date prefix
-        querySet = model_instance.__class__.objects.filter(**{
-            F"{self.name}__startswith": date_pst.strftime('%y%m%d')
-        })
-        last_object = querySet.order_by(F"-{self.name}").first()
-
-        # Increment the sequence number
-        sequence_number = 1
-        if last_object:
-            last_sequence_number = int(last_object.__dict__[self.name][9:])
-            sequence_number = last_sequence_number + 1
-
-        # Set the unique identifier field value
-        model_instance.__dict__[self.name] = '{}-{:03d}'.format(date_pst.strftime('%y%m%d'), sequence_number)
+            # Set the unique identifier field value
+            model_instance.__dict__[self.name] = '{}-{:03d}'.format(date.strftime('%y%m%d'), sequence_number)
 
         return model_instance.__dict__[self.name]
