ff144caa6beefa2052e12448c574c5ae8c39a77d
Elias Fung <manchai.fung@gmail.com>
2024-02-01 16:46:29 -0400

    fix: returned order_by

--- core_api_filter/views.py
+++ core_api_filter/views.py
@@ -277,8 +277,8 @@ class ManageEventsMixin:
                     unique_filtered.append(item)
 
             sorted_filtered = unique_filtered
-            filterQueryset = ExtraQuerySet(Event).filter(id__in=sorted_filtered)
-            
+            filterQueryset = ExtraQuerySet(Event).filter(id__in=sorted_filtered).order_by('-id')
+
             if 'order_to_sort' in request.GET and 'field_to_sort' in request.GET:
                 order_to_sort = request.GET.get('order_to_sort')
                 field_to_sort = request.GET.get('field_to_sort')
