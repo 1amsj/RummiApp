feb058a4311c3efb65adbc66ffc49f8b1e12a3ac
Marcel <marcelvallejo2021@gmail.com>
2024-02-29 14:47:31 -0400

    Renamed API key

--- core_api/constants.py
+++ core_api/constants.py
@@ -38,7 +38,7 @@ class ApiSpecialKeys(str, Enum):
     REQUESTER_DATA = '_requester_data'
     SERVICE_DATALIST = '_service_datalist'
     SERVICE_AREA_DATALIST = '_service_area_datalist'
-    COMPANY_RELATIONSHIPS_DATALIST = '_preferred_agency_datalist'
+    COMPANY_RELATIONSHIPS_DATA = '_preferred_agency_data'
 
 
 
