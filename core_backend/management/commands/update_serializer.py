8428b08a95acc095fe54785f2c9698958fac39f4
gabriel <gabrielchacon200269@gmail.com>
2024-04-11 15:43:02 -0400

    Add Timer and Count

--- core_backend/management/commands/update_serializer.py
+++ core_backend/management/commands/update_serializer.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import django
 
 django.setup()
@@ -5,7 +6,17 @@ django.setup()
 from django.core.management.base import BaseCommand
 from core_backend.models import Authorization, Booking, Event, Report
 
+pending_count = 0
+booked_count = 0 
+delivered_count = 0
+override_count = 0
+authorized_count = 0
+error_count = 0
+error_ids = []
+
 class Command(BaseCommand):
+
+    start = datetime.now()
 
     def validator_type(value):
         return (value.payer_company.type == 'insurance' and value.payer is not None and value.payer_company is not None) or \
@@ -44,32 +55,53 @@ class Command(BaseCommand):
                     and validator_type(event_datalist) \
                     and event_get_extra.__contains__('claim_number') is not None:
                         booking.status = "delivered"
+                        delivered_count += 1
                     elif aut_list.count() > 0:
                         aut_list_status = aut_list._hints['instance'].status
                         if aut_list_status == 'ACCEPTED' and validator_short_type(event_datalist):
                             booking.status = "authorized"
+                            authorized_count += 1
                         elif aut_list_status == 'OVERRIDE' and validator_short_type(event_datalist):
                             booking.status = "override"
+                            override_count += 1
                         elif event_get_extra.__contains__('claim_number') is not None and validator_type(event_datalist):
                             booking.status = "booked"
+                            booked_count += 1
                         else:
                             booking.status = "pending"
+                            pending_count += 1
                     elif event_get_extra.__contains__('claim_number') is not None and validator_type(event_datalist):
                         booking.status = "booked"
+                        booked_count += 1
                     else:
                         booking.status = "pending"
+                        pending_count += 1
                 else:
                     if list_report_status != [] and list_report_status[-1] == 'COMPLETED' \
                     and validator_clinic_type(event_datalist) \
                     and event_get_extra.__contains__('claim_number') is not None:
                         booking.status = "delivered"
+                        delivered_count += 1
                     elif event_get_extra.__contains__('claim_number') is not None and validator_clinic_type(event_datalist):
                         booking.status = "booked"
+                        booked_count += 1
                     else:
                         booking.status = "pending"
+                        pending_count += 1
                         
                 booking.save()
         except:
+            error_count += 1
+            error_ids.append(booking.public_id)
             print(f"Error creating booking_status: {booking.public_id}")
     
             continue
+        
+        end = datetime.now()
+        duration = end - start
+
+        print(f"Pending: {pending_count}")
+
+        print(f"Errores: {error_count}. IDs: {error_ids}")
+
+        print(f"Tiempo: {duration}")
