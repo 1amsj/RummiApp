371ddd26cb66fd8261c5a5cbbcd151703fce3a54
gabriel <gabrielchacon200269@gmail.com>
2024-06-28 12:56:47 -0400

    Fix

--- core_api/services_datamanagement.py
+++ core_api/services_datamanagement.py
@@ -508,7 +508,7 @@ def update_company_relationship_wrap(data, company_id, company_relationship_inst
 
 
 @transaction.atomic
-def update_event_wrap(data, business_name, group_booking, event_instance):
+def update_event_wrap(data, business_name, event_instance, group_booking = False):
     if not business_name:
         raise BusinessNotProvidedException
 
