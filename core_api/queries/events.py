c4e29b5a38f35829f073cad33dc8c9692d8f0bb0
Starloss <starloss09@gmail.com>
2024-09-12 14:32:32 -0400

    Implemented improved query method for events

--- core_api/queries/events.py
+++ core_api/queries/events.py
@@ -1,15 +1,63 @@
 class ApiSpecialSqlEvents():
+
+    @staticmethod
+    def get_event_sql_where_clause(id, limit, offset):
+        params = []
+        limit_statement = ''
+        where_conditions = 'event.is_deleted = FALSE'
+
+        if id is not None:
+            where_conditions += ' AND event.id = %s'
+            params.append(id)
+
+        if limit is not None and limit > 0 and offset is not None and offset >= 0:
+            limit_statement = 'LIMIT %s OFFSET %s'
+            params.append(limit)
+            params.append(offset)
+
+        return params, where_conditions, limit_statement
     
-    def get_event_sql(event_id):
-        return """
-            SELECT json_build_object(
-                'id', event.id,
-                'is_deleted', event.is_deleted,
-                'start_at', event.start_at,
-                'end_at', event.end_at,
-                'arrive_at', event.arrive_at,
-                'description', event.description
-            )
-            FROM core_backend_event event
-            WHERE event.id = """ + str(event_id) + """
-        """
+    @staticmethod
+    def get_event_sql(cursor, id, limit, offset):
+        params, where_conditions, limit_statement = ApiSpecialSqlEvents.get_event_sql_where_clause(id, limit, offset)
+
+        query = """
+            SELECT json_agg(row_to_json(_query_result)) AS result FROM (
+                SELECT
+                    event.id,
+                    event.is_deleted,
+                    event.start_at,
+                    event.end_at,
+                    event.arrive_at,
+                    event.description
+                FROM "core_backend_event" event
+                WHERE %s
+                ORDER BY event.start_at, event.id DESC
+                %s
+            ) _query_result
+        """ % (where_conditions, limit_statement)
+
+        cursor.execute(query, params)
+        result = cursor.fetchone()
+        if len(result) == 1:
+            return result[0]
+
+        return []
+    
+    @staticmethod
+    def get_event_count_sql(cursor, id):
+        params, where_conditions, _ = ApiSpecialSqlEvents.get_event_sql_where_clause(id, None, None)
+
+        query = """
+            SELECT
+               COUNT(*)
+            FROM "core_backend_event" event
+            WHERE %s
+        """ % where_conditions
+
+        cursor.execute(query, params)
+        result = cursor.fetchone()
+        if len(result) == 1:
+            return result[0]
+
+        return 0
