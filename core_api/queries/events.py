7622b73e2cbde6878929ff2c62aa62f1437ff3b9
Starloss <starloss09@gmail.com>
2024-11-20 17:03:09 -0400

    Added interpreter as filter
    Fixed JOINS on get_event_count_sql from INNER to LEFT, allowing to count correctly the ammount of events

--- core_api/queries/events.py
+++ core_api/queries/events.py
@@ -77,6 +77,12 @@ class ApiSpecialSqlEvents():
             params.append('payer_company_type')
             params.append('clinic')
             params.append('patient')
+
+        if 'interpreter' in pending_items_included:
+            where_conditions += ' AND provider.id IS NULL'
+
+        if 'interpreter' in pending_items_excluded:
+            where_conditions += ' AND provider.id IS NOT NULL'
             
         if recipient_id is not None and agent_id is None:
             where_conditions += ' AND recipient.id = %s'
@@ -368,13 +374,13 @@ class ApiSpecialSqlEvents():
                     ON booking_companies.booking_id = booking.id
                 INNER JOIN "core_backend_company" company
                     ON company.id = booking_companies.company_id
-                INNER JOIN "core_backend_booking_services" booking_services
+                LEFT JOIN "core_backend_booking_services" booking_services
                     ON booking_services.booking_id = booking.id
-                INNER JOIN "core_backend_service" service
+                LEFT JOIN "core_backend_service" service
                     ON service.id = booking_services.service_id
-                INNER JOIN "core_backend_provider" provider
+                LEFT JOIN "core_backend_provider" provider
                     ON provider.id = service.provider_id
-                INNER JOIN "core_backend_user" provider_user
+                LEFT JOIN "core_backend_user" provider_user
                     ON provider_user.id = provider.user_id
                 INNER JOIN "core_backend_event_affiliates" event_affiliates
                     ON event_affiliates.event_id = event.id
