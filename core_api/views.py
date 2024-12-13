ef746f015dc7b5593550c1a32ebfd5c904e4acff
Starloss <starloss09@gmail.com>
2024-12-13 19:49:25 -0400

    Added providers query

--- core_api/views.py
+++ core_api/views.py
@@ -28,6 +28,7 @@ from core_api.queries.events import ApiSpecialSqlEvents
 from core_api.queries.companies import ApiSpecialSqlCompanies
 from core_api.queries.affiliations import ApiSpecialSqlAffiliations
 from core_api.queries.operators import ApiSpecialSqlOperators
+from core_api.queries.providers import ApiSpecialSqlProviders
 from core_api.queries.reports import ApiSpecialSqlReports
 from core_api.queries.service_root import ApiSpecialSqlServiceRoot
 from core_api.queries.services import ApiSpecialSqlServices
@@ -773,6 +774,66 @@ class ManageProviders(user_subtype_view_manager(Provider, ProviderSerializer)):
     @classmethod
     @expect_does_not_exist(Provider)
     def get(cls, request, business_name=None, provider_id=None):
+        testing = request.GET.get('testing', False)
+        
+        if testing == True:
+            query_param_field_to_sort = request.GET.get('field_to_sort', None)
+            query_param_order_to_sort = request.GET.get('order_to_sort', None)
+            query_param_id = request.GET.get('id', None)
+            
+            query_provider_id = provider_id if provider_id is not None else query_param_id
+            query_field_to_sort = 'provider_user.first_name'
+            query_order_to_sort = 'DESC' if query_param_order_to_sort == 'asc' else 'ASC'
+                
+            try:
+                query_param_page_size = int(request.GET.get('page_size', '-1'))
+            except:
+                raise ParseError(detail='invalid "page_size" in the query parameters', code=None)
+
+            try:
+                query_param_page = int(request.GET.get('page', '1'))
+            except:
+                raise ParseError(detail='invalid "page" in the query parameters', code=None)
+
+            offset = ((query_param_page - 1) * query_param_page_size) if (query_param_page_size > 0 and query_param_page > 0) else 0
+            
+            with connection.cursor() as cursor:
+                result = ApiSpecialSqlProviders.get_provider_sql(
+                    cursor,
+                    query_provider_id,
+                    query_param_page_size,
+                    offset,
+                    query_field_to_sort,
+                    query_order_to_sort
+                )
+
+            if query_param_page_size > 0:
+                with connection.cursor() as cursor:
+                    count = ApiSpecialSqlProviders.get_provider_count_sql(
+                        cursor,
+                        query_provider_id
+                    )
+
+                next_page = query_param_page + 1 if (count > (query_param_page_size * query_param_page)) else None
+                if next_page is not None:
+                    next_page = replace_query_param(request.build_absolute_uri(), 'page', next_page)
+
+                previous_page = query_param_page - 1 if query_param_page > 1 else None
+                if previous_page is not None:
+                    previous_page = replace_query_param(request.build_absolute_uri(), 'page', previous_page)
+
+                return Response({
+                    'count': count,
+                    'next': next_page,
+                    'previous': previous_page,
+                    'results': result
+                })
+                
+            if (query_provider_id is not None):
+                return Response(result[0])
+            
+            return Response(result)
+        
         if provider_id:
             provider = Provider.objects.all().not_deleted('user').get(id=provider_id)
             serialized = ProviderSerializer(provider)
