b0f786bdd8c05a79e24852da51bbedaa465536c4
giselm30 <132848883+giselm30@users.noreply.github.com>
2025-04-22 17:46:05 -0400

    Update provider ID handling in ManageEventsMixin for improved user context

--- core_api/views.py
+++ core_api/views.py
@@ -1275,6 +1275,15 @@ class ManageEventsMixin:
         query_param_report = request.GET.get('report', False)
         query_param_id = request.GET.get('id', None)
         
+        user = request.user
+        
+        if query_param_provider_id is not None:
+            query_provider_id = query_param_provider_id
+        elif user.is_provider and not user.is_operator:
+            query_provider_id = user.as_provider.id
+        else:
+            query_provider_id = None
+            
         query_event_id = event_id if event_id is not None else query_param_id
         query_start_at = datetime.strptime(query_param_start_at, "%Y-%m-%dT%H:%M:%S.%f%z").astimezone(pytz.utc) if query_param_start_at is not None else None
         query_end_at = datetime.strptime(query_param_end_at, "%Y-%m-%dT%H:%M:%S.%f%z").astimezone(pytz.utc) if query_param_end_at is not None else None
@@ -1318,7 +1327,7 @@ class ManageEventsMixin:
                     query_param_items_excluded,
                     query_param_recipient_id,
                     query_param_agent_id,
-                    query_param_provider_id,
+                    query_provider_id,
                     query_param_start_date,
                     query_param_end_date,
                     query_param_provider_name,
@@ -1390,7 +1399,7 @@ class ManageEventsMixin:
                     query_param_items_excluded,
                     query_param_recipient_id,
                     query_param_agent_id,
-                    query_param_provider_id,
+                    query_provider_id,
                     query_param_start_date,
                     query_param_end_date,
                     query_param_provider_name,
@@ -1414,7 +1423,7 @@ class ManageEventsMixin:
                     query_param_items_excluded,
                     query_param_recipient_id,
                     query_param_agent_id,
-                    query_param_provider_id,
+                    query_provider_id,
                     query_param_start_date,
                     query_param_end_date,
                     query_param_provider_name,
