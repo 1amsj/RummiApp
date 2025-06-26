87d029af7969d1d9ecd4f007e1553474335d49e9
1amsj <149028405+1amsj@users.noreply.github.com>
2025-06-25 18:39:31 -0600

    feat: replace NewTask component with AddAnimal component in App

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import MainScreen from './components/MainScreen';
 import Listado from './components/Listado';
 import NewTask from './components/NewTask';
+import AddAnimal from './components/AddAnimal';
 
 export default function App() {
-  return <NewTask />;
+  return <AddAnimal />;
 }
