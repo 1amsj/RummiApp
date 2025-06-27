255af61aab94fbc16557b99e78e6913e1e132080
1amsj <149028405+1amsj@users.noreply.github.com>
2025-06-26 19:43:38 -0600

    feat: enhance Vacunas component layout and add header structure

--- components/Vacunas.js
+++ components/Vacunas.js
@@ -6,11 +6,40 @@ import { Image } from 'react-native';
 export default function Vacunas(){
     return(
         <View style = {styles.container}>
+            <View style={styles.container1}>
+                    {/* Header */}
+                <View style={styles.headerContainer}>
+                    <TouchableOpacity style={styles.MenuButton} onPress={() => navigation.openDrawer()}>
+                        <View style={styles.linea} />
+                        <View style={styles.linea} />
+                        <View style={styles.linea} />
+                    </TouchableOpacity>
+                      <View style={styles.TextContainer}>
+                        <Text style={styles.headerText}>
+                          RummiApp
+                        </Text>
+                      </View>
+                    </View>
+                </View>
             <StatusBar style = "auto"/>
         </View>
         );
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    gap: 60,
+  },
+  container1: {
+    flex: 1,
+    backgroundColor: '#fff',
+    gap: 25,
+  },
+  container2: {
+    flex: 2,
+    backgroundColor: '#fff',
+    gap: 20,
+  },
 });
